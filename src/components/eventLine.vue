b97863d1acc7614b1934a684fc48b903a554a6d5
Alex Regan <alex.joseph.regan@gmail.com>
2019-10-02 17:15:49 -0700

    feat: title hover

--- src/components/eventLine.vue
+++ src/components/eventLine.vue
@@ -1,85 +1,74 @@
 <template>
-  <span
-    class="eventLine"
-    :title="toolTipInformation"
-    @mouseover="createTitleInformation(event)"
-  >|
-    <a href="#" :title="toolTipInformation" class="tooltip"></a>
+  <span class="eventLine" :data-title="toolTipInformation" @mouseover="createTitleInformation(event)">
+    |
+    <a href="#" :data-title="toolTipInformation" class="tooltip"></a>
   </span>
 </template>
 
 <script>
-  import { Stage } from "../models/Stage";
-  export default {
-    name: "eventLine",
-    props: {
-      event: {
-        type: Object,
-        required: true
-      },
-      stage: {
-        type: Object,
-        required: true
-      },
-      projectStart: {
-        type: Date,
-        required: true
-      },
-      projectEnd: {
-        type: Date,
-        required: true
-      }
+import { Stage } from "../models/Stage";
+export default {
+  name: "eventLine",
+  props: {
+    event: {
+      type: Object,
+      required: true
     },
-    data: () => {
-      return {
-        toolTipInformation:{
-
-        }
-      }
+    stage: {
+      type: Object,
+      required: true
     },
-    methods: {
-      createTitleInformation(event){
-        console.log(event.name);
-        this.eventInformation = event.name;
-
-      }
+    projectStart: {
+      type: Date,
+      required: true
     },
-    computed: {
-
-    },
-    watch: {},
-    created() {
+    projectEnd: {
+      type: Date,
+      required: true
     }
-
-
-  }
+  },
+  data: () => {
+    return {
+      toolTipInformation: "thingy"
+    };
+  },
+  methods: {
+    createTitleInformation(event) {
+      console.log(event.name);
+      this.eventInformation = event.name;
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {}
+};
 </script>
 
 <style scoped>
 .eventLine {
   font-size: 1.3em;
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
   line-height: unset;
 }
-  .tooltip {
-    display: inline;
-    position: relative;
-  }
-.tooltip:hover:after{
+.tooltip {
+  display: inline;
+  position: relative;
+}
+.tooltip:hover:after {
   background: #333;
-  background: rgba(0,0,0,.8);
+  background: rgba(0, 0, 0, 0.8);
   border-radius: 5px;
   bottom: 26px;
   color: #fff;
-  content: attr(title);
+  content: attr(data-title);
   left: 20%;
   padding: 5px 15px;
   position: absolute;
   z-index: 98;
   width: 220px;
 }
-.tooltip:hover:before{
+.tooltip:hover:before {
   border: solid;
   border-color: #333 transparent;
   border-width: 6px 6px 0 6px;
